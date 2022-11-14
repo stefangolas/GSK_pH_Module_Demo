ec8d54af129106db661889c85d78dd0166b3e92e
Stefan Golas <stefanmgolas@gmail.com>
2022-11-13 19:12:40 -0500

    Updated with integer indexing on wells

--- ph_demo.py
+++ ph_demo.py
@@ -7,15 +7,18 @@ Created on Sun Jul 17 21:12:47 2022
 import os
 from pyhamilton import (HamiltonInterface,  LayoutManager, normal_logging, 
                         initialize, move_sequence, get_plate_gripper_seq, place_plate_gripper_seq,
-                        move_plate_gripper_seq)
+                        move_plate_gripper_seq, ResourceType, Plate96)
 
 import logging
 
 from ph_utils import pH_Module
+import sys
 
-import csv
 
 lmgr = LayoutManager('Demo.lay')
+
+sample_plate = lmgr.assign_unused_resource(ResourceType(Plate96, 'Cos_96_Rd_0001'))
+
 
 default_temperature = 25.0
 calibration_time = 30.0
@@ -29,33 +32,27 @@ if __name__ == '__main__':
         #pH Module Commands
         
         ph_module = pH_Module(ham_int, module_id = 0, comport = 1, simulate = True, default_temperature = 25.0)
-                
+        
+
         get_plate_gripper_seq(ham_int, 'seqpH_Module_0001', 39.0, 81.0, 90.0, 
                               lid = False, tool_sequence = 'COREGripTool_1000ul_0001')
         
-        ph_module.calibration(solution_seq = 'seqCalibration_1', 
-                              solution_ph = 4.0, 
-                              dynamic_calibration = False, 
-                              probe_pattern = '1111',
-                              calibration_time = 1)
+        #ph_module.calibration(solution_seq = 'seqCalibration_1', 
+        #                      solution_ph = 4.0, 
+        #                      dynamic_calibration = False, 
+        #                      probe_pattern = '1111',
+        #                      calibration_time = 5)
         
-        ph_module.calibration(solution_seq = 'seqCalibration_2', 
-                              solution_ph = 8.0, 
-                              dynamic_calibration = False, 
-                              probe_pattern = '1111',
-                              calibration_time = 1)
+        #ph_module.calibration(solution_seq = 'seqCalibration_2', 
+        #                      solution_ph = 8.0, 
+        #                      dynamic_calibration = False, 
+        #                      probe_pattern = '1111',
+        #                      calibration_time = 5)
         
-        output = ph_module.measure(solution_seq, '1111')
+        output = ph_module.measure([(sample_plate, 0)], '1111') #Integer indexing
         
         place_plate_gripper_seq(ham_int, 'seqpH_Module_0001', tool_sequence = 'COREGripTool_1000ul_0001')
 
-        calibration_data_list = []
-        for i in range(4):
+        for i in range(1,5):
             data = ph_module.request_calibration(probe_number = i)
-            calibration_data_list.append(data)
-        
-        with open('dict.csv', 'w') as csv_file:  
-            writer = csv.writer(csv_file)
-            for key, value in calibration_data_list[0].items():
-                writer.writerow([key, value])
-
+            print(data)
--- ph_utils.py
+++ ph_utils.py
@@ -8,13 +8,14 @@ Created on Sun Oct 23 21:48:52 2022
 import os
 from pyhamilton import (HamiltonInterface,  LayoutManager, normal_logging, 
                         initialize, move_sequence, get_plate_gripper_seq, place_plate_gripper_seq,
-                        move_plate_gripper_seq)
+                        move_plate_gripper_seq, move_plate_gripper)
 
-from pyhamilton.pH_command_wrappers import (ph_washer_wash,ph_dryer_start, ph_dryer_stop, ph_calibrate, ph_sleep,
-                                            ph_wakeup, ph_calibrate_dynamically, 
+from pyhamilton import (ph_washer_wash,ph_dryer_start, ph_dryer_stop, ph_calibrate, ph_sleep,
+                                            ph_wakeup, ph_calibrate_dynamically, ph_dryer_initialize, ph_washer_initialize,
                                             ph_measure, ph_initialize, ph_request_calibration)
 import logging
 import time
+import sys
 
 
 
@@ -25,9 +26,28 @@ class pH_Module():
         self.module_id = module_id
         self.default_temperature = default_temperature
         ph_initialize(self.hamilton, comport = comport, simulate = simulate)
+        self.washer_module = ph_washer_initialize(self.hamilton, comport = comport, simulate=simulate)
+        self.dryer_module = ph_dryer_initialize(self.hamilton, comport = comport, simulate=simulate)
+        self.xdis = 0.0
+        self.ydis = 0.0
+        self.zdis = 0.0
         
-    def measure(self, solution_seq, probe_pattern):
-        move_plate_gripper_seq(self.hamilton, solution_seq)
+    def handle_exceptions(f):
+        from functools import wraps
+        @wraps(f)
+        def wrapper(self, *args, **kw):
+           try:
+               return f(self, *args, **kw)
+           except Exception as e:
+               print(str(e))
+               place_plate_gripper_seq(self.hamilton, 'seqpH_Module_0001', tool_sequence='COREGripTool_1000ul_0001')
+               sys.exit()
+        return wrapper
+
+        
+    def measure(self, solution_pos, probe_pattern):
+        move_plate_gripper(self.hamilton, solution_pos, xDisplacement = self.xdis,
+                           yDisplacement = self.ydis, zDisplacement = self.zdis)
         ph_wakeup(self.hamilton, self.module_id)
         output = ph_measure(self.hamilton, 
                             module_id = self.module_id, 
@@ -37,20 +57,21 @@ class pH_Module():
         self.wash_and_dry('seqWash_Module', 'seqDryer_Module', cycle_num = 2, dry_time = 3)
         return output
     
+    @handle_exceptions
     def wash_and_dry(self, wash_seq, dry_seq, cycle_num, dry_time):
         move_plate_gripper_seq(self.hamilton, wash_seq)
         ph_washer_wash(self.hamilton, module_id=self.module_id, cycle_num = cycle_num)
         move_plate_gripper_seq(self.hamilton, dry_seq)
-        ph_dryer_start(self.hamilton, module_id = self.module_id)
+        ph_dryer_start(self.hamilton, module_id = 100)
         time.sleep(dry_time)
         move_sequence(self.hamilton, dry_seq, 0, 0, 20)
         move_plate_gripper_seq(self.hamilton, dry_seq)
         time.sleep(dry_time)
-        ph_dryer_stop(self.hamilton, self.module_id)
+        ph_dryer_stop(self.hamilton, module_id = 100)
         ph_sleep(self.hamilton, module_id = self.module_id)
-        
+      
+    @handle_exceptions
     def calibration(self, solution_seq, solution_ph, dynamic_calibration, probe_pattern, calibration_time):
-        
         move_plate_gripper_seq(self.hamilton, solution_seq)
         ph_wakeup(self.hamilton, module_id = self.module_id)
             #_Success or moveback        
@@ -61,7 +82,7 @@ class pH_Module():
                          cal_level = 0, 
                          cal_value = solution_ph, 
                          cal_temperature = self.default_temperature, 
-                         probe_pattern = '1111')
+                         probe_pattern = probe_pattern)
             
             
         else:
@@ -72,20 +93,11 @@ class pH_Module():
                                      cal_level = 0, 
                                      cal_value = solution_ph, 
                                      cal_temperature = self.default_temperature,
-                                     probe_pattern = '1111')
+                                     probe_pattern = probe_pattern)
         
             self.wash_and_dry('seqWash_Module', 'seqDryer_Module', cycle_num = 2, dry_time = 3)
             ph_sleep(self.hamilton, module_id = 0)
     
     def request_calibration(self, probe_number):
-        calibration_dictionary = {}
-        calibration_data = ph_request_calibration(self.hamilton, self.module_id, probe_number)
-        calibration_dictionary['LowValue'] = calibration_data[0]
-        calibration_dictionary['LowVoltage'] = calibration_data[1]
-        calibration_dictionary['HighValue'] = calibration_data[2]
-        calibration_dictionary['HighVoltage'] = calibration_data[3]
-        calibration_dictionary['Temperature'] = calibration_data[4]
-        calibration_dictionary['Offset'] = calibration_data[5]
-        calibration_dictionary['Slope'] = calibration_data[6]
-        calibration_dictionary['Ratio'] = calibration_data[7]
-        return calibration_dictionary
+        calibration_data = ph_request_calibration(self.hamilton, 1000, probe_number)
+        return calibration_data
